439d25a11892ffc2251313d6ebee952120d82bd3
Heverton Vidal Paulino <75618512+Hevertonvp@users.noreply.github.com>
2025-09-05 10:17:38 -0300

    layout blog alterado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,8 @@
-import './Footer.css'
 
 function Footer() {
     return (
         <footer>
-            <div class="footer-content">
+            <div className="footer-content h-[150px] w-full bg-[#0A2647] text-white flex flex-col items-center justify-center gap-2 p-4 text-center">
                 <p>&copy; 2024 Biblioteca Escola Dep. Oliveira Souza. Todos os direitos reservados.</p>
                 <p>Desenvolvido pelos alunos do curso técnico em informática, 3º ano</p>
             </div>
